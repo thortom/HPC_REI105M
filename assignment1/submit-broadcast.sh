ee52bc7d2da05f0a42dc110a48a252a5d176772e
Þór Tómasarson <tht33@jotunn.rhi.hi.is>
2017-09-21 23:50:02 +0000

    Adding solution to part 1.3

--- assignment1/submit-broadcast.sh
+++ assignment1/submit-broadcast.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -J Broadcast-Test
+#SBATCH -J ScatterGather-Test
 #SBATCH -n 4
 #SBATCH --mail-user=tht33@hi.is
 #SBATCH --mail-type=end
 module load gnu openmpi
-mpirun /home/tht33/assignment1/broadcast
+mpirun /home/tht33/HPC_REI105M/assignment1/broadcast
