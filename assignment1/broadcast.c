f9ac9a19931ce5b4611949277054ee805d5d574c
Þór Tómasarson <tht33@hi.is>
2017-09-22 17:43:57 +0000

    Small modification to 1.3

--- assignment1/broadcast.c
+++ assignment1/broadcast.c
@@ -21,9 +21,6 @@ int main (int argc, char *argv[])
     int elements_per_proc = 4;
     int array[elements_per_proc * world_size];
     int sub_array[elements_per_proc];
-
-    MPI_Status status;
-    MPI_Request request;
     
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
