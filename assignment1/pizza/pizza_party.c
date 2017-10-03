8839710133f728edf600e97ba398e844f3ade93e
Þór Tómasarson <tht33@hi.is>
2017-10-03 11:27:38 +0000

    Splitting the pizza project

--- assignment1/pizza/pizza_party.c
+++ assignment1/pizza/pizza_party.c
@@ -4,125 +4,8 @@
 #include <math.h>
 #include <time.h>
 #include <unistd.h>
-
-typedef int Gender;
-#define male 1
-#define female 2
-
-typedef int bool;
-#define true 1
-#define false 0
-
-/* global variable declaration */
-int action_time = 1000 * 1000;  /* One second*/
-
-/* TODO: Move Person in to separate file */
-typedef struct PersonIdentity {
-    int rank;
-    Gender gender;
-    int group_rank;
-    int pepperoni;
-    int pizza_size;
-    int is_window_open;
-} Person;
-
-void Person_constructor(Person *me, int rank, int gender,
-                            int group_rank) {
-    me->rank = rank;
-    me->gender = gender;
-    me->group_rank = group_rank;
-    /* random int between 2 and 20 */
-    me->pepperoni = (rand() % 19) + 2;
-    /* random int between 6 and 10 */
-    me->pizza_size = (rand() % 5) + 6;
-    me->is_window_open = false;
-}
-
-int get_pepperoni_count(Person *me) {
-    return me->pepperoni;
-}
-
-bool done_with_pizza(Person *me) {
-    if (me->pizza_size == 0)
-        return true;
-    return false;
-}
-
-void take_bite_of_pizza(Person *me) {
-    if (me->pizza_size > 0) {
-        usleep(action_time);
-        me->pizza_size -= 1;
-    }
-}
-
-void open_window(Person *me) {
-    if (me->is_window_open) {
-        printf("The window is already open\n");
-    }
-    else {
-        usleep(action_time);
-        me->is_window_open = true;
-    }
-}
-
-void close_window(Person *me) {
-    if (!me->is_window_open) {
-        printf("The window is already closed\n");
-    }
-    else {
-        usleep(action_time);
-        me->is_window_open = true;
-    }
-}
-
-void answer_phone(Person *me) {
-    printf("Answering the phone\n");
-    usleep(action_time);
-    printf("Done talking\n");
-}
-
-char* get_group_name(Gender gender) {
-    if (gender == female)
-        return "female";
-    else
-        return "male";
-}
-
-void get_my_name(Person *me, char* my_name) {
-    if (me->rank == 0)
-        snprintf(my_name, 20, "the host");
-    else    
-        snprintf(my_name, 20, "%s, %d",
-                        get_group_name(me->gender), me->group_rank);
-}
-
-void print_info(Person *me) {
-    if (me->rank == 0)
-        printf("The host with world rank 0\n");
-    else
-        printf("Person with world rank %d and group rank %d in %s group\n",
-                    me->rank, me->group_rank, get_group_name(me->gender));
-    printf("    I got %d pepperoni on my pizza\n", me->pepperoni);
-    printf("    My pizza is %d bites\n", me->pizza_size);
-}
-
-/* TODO: Maybe move these global functions in to separate file as well*/
-bool random_bool() {
-    return rand() % 2;
-}
-
-bool is_hot() {
-    return random_bool();
-}
-
-bool is_cold() {
-    return random_bool();
-}
-
-bool phone_is_rinnging() {
-    return random_bool();
-}
-
+#include "person.h"
+#include "environment.h"
 
 int main (int argc, char *argv[])
 {
@@ -221,4 +104,5 @@ int main (int argc, char *argv[])
     MPI_Finalize();
     return 0;
 }
+
 
