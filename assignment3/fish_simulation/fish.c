e6f515ce5da472b7d84b20971dac7a585f4747d4
Þór Tómasarson <tht33@hi.is>
2017-11-24 11:20:20 +0000

    Starting on fish stuff

--- assignment3/fish_simulation/fish.c
+++ assignment3/fish_simulation/fish.c
@@ -1,18 +1,19 @@
 #include "fish.h"
+#include "logger.h"
 
-void fish_group_constructor(fish_group *me, int group_number);
+void fish_group_constructor(fish_group *me, int group_number)
 {
     me->group_number = group_number;
     /* random int between 10 and 20 */
     me->numb_fish = (rand() % 11) + 10;
 }
 
-void print_fish_group(fish_group *me);
+void print_fish_group(fish_group *me)
 {
-    log_info("Fish group number: %d, contains: %d fish", me->fish_group, me->numb_fish);
+    log_info("Fish group number: %d, contains: %d fish", me->group_number, me->numb_fish);
 }
 
-int get_current_direction(fish_group *me);
+int get_current_direction(fish_group *me)
 {
     /* Up, Down, Left, Right */
     int direction = rand() % 4;
