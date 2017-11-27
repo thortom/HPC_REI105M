d5d25cd4c4aa1dda8a2112b652a89f7f4fe51a6d
siggigauti <siggigauti@gmail.com>
2017-11-27 14:40:58 +0000

    added newline to make binary more readable

--- assignment3/fish_simulation/ocean.c
+++ assignment3/fish_simulation/ocean.c
@@ -481,7 +481,7 @@ int main (int argc, char** argv)
             num_boats = 1;
         }
         char buff[200];
-        sprintf(buff, "Node %d: has %d fish groups, and %d boat(s).", node.rank, num_fish, num_boats);
+        sprintf(buff, "Node %d: has %d fish groups, and %d boat(s).\n", node.rank, num_fish, num_boats);
         int len = sizeof(buff)/sizeof(int);
         MPI_File_open(MPI_COMM_WORLD, "test", MPI_MODE_CREATE|MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
         MPI_File_write_ordered(fh, buff, len, MPI_CHAR, &status);
